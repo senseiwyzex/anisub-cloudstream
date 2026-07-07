37e56e2df9d7a9d5b4d6bd67ff236d261a7efd14
senseiwyzex <wyzexsensei2@gmail.com>
2026-07-07 11:52:55 +0400

    Update ExamplePlugin.kt

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -6,20 +6,15 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class ExamplePlugin: Plugin() {
+class ExamplePlugin : Plugin() {
+
     private var activity: AppCompatActivity? = null
 
     override fun load(context: Context) {
+
         activity = context as? AppCompatActivity
 
-        // All providers should be added in this manner
         registerMainAPI(ExampleProvider())
 
-        openSettings = {
-            val frag = BlankFragment(this)
-            activity?.let {
-                frag.show(it.supportFragmentManager, "Frag")
-            }
-        }
     }
 }
