c4927e566c4fcd04a68e3334cfb84871768b7376
senseiwyzex <wyzexsensei2@gmail.com>
2026-07-07 11:53:33 +0400

    Update AniSubApi.kt

--- ExampleProvider/src/main/kotlin/com/example/AniSubApi.kt
+++ ExampleProvider/src/main/kotlin/com/example/AniSubApi.kt
@@ -1,7 +1,17 @@
 package com.example
 
+import com.lagradost.nicehttp.Requests
+
 object AniSubApi {
 
-    const val API = "https://anisub.co/api"
+    private const val API = "https://anisub.co/api"
+
+    suspend fun ping(): String {
+
+        return Requests()
+            .get("$API/subtitles")
+            .text
+
+    }
 
 }
