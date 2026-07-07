e51fdd1e5541f47c43fc366f3e29a967a2af36bc
senseiwyzex <wyzexsensei2@gmail.com>
2026-07-07 11:49:57 +0400

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -3,31 +3,26 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 }
 
-// Use an integer for version numbers
 version = 1
 
 cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
+    description = "AniSub Turkish Subtitle Provider"
 
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst", "Luna712")
+    authors = listOf(
+        "Wyzex"
+    )
 
-    /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
-    status = 1 // Will be 3 if unspecified
+    status = 1
 
-    tvTypes = listOf("Movie")
+    tvTypes = listOf(
+        "Anime"
+    )
 
     requiresResources = true
-    language = "en"
 
-    // Random CC logo I found
-    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
+    language = "tr"
+
+    iconUrl = "https://anisub.co/favicon.ico"
 }
 
 android {
